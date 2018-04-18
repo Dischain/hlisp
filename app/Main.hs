4ce7fd53e6ecf38fb1f7cbc394dc4be5c8d9a2b2
Aleksandr Shaytanov <rdischain@yandex.ru>
2018-04-19 00:26:11 +0300

    update for vars

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,6 @@ import Repl
 main :: IO ()
 main = do args <- getArgs
           case length args of
-            0 -> runRepl
-            1 -> evalAndPrint $ args !! 0
-            otherwise -> putStrLn "Program takes only 0 or 1 argument"
+               0 -> runRepl
+               1 -> runOne $ args !! 0
+               otherwise -> putStrLn "Program takes only 0 or 1 argument"
